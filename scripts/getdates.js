03afb5458179b012485daf701f0138ffac6f7844
AcXirus <dtorresflor@byupathway.edu>
2025-07-05 11:32:49 -0500

    finish home page

---
+++ scripts/getdates.js
@@ -0,0 +1,16 @@
+const currentYear = new Date().getFullYear();
+
+const footerParagraph = document.querySelector('footer p');
+
+if (footerParagraph) {
+  footerParagraph.textContent = `© ${currentYear} || Dillan Torres || Ecuador `;
+}
+
+alert(document.lastModified);
+
+let nLastModif = Date.parse(document.lastModified);
+let readableDate = new Date(nLastModif);
+
+document.getElementById("lastModified").textContent = 
+  "Last Modification: " + readableDate.toLocaleString();
+
